0fd02771bde2c6413a719285be98d9f8769a244a
raulsfcp4 <36735937+raunavcru@users.noreply.github.com>
2019-11-17 17:20:16 +0100

    borrar plugin

--- wp-content/plugins/ultimate-member/templates/message.php
+++
@@ -1,9 +0,0 @@
-<div class="um <?php echo $this->get_class( $mode ); ?> um-<?php echo esc_attr( $form_id ); ?>">
-
-	<div class="um-postmessage">
-		
-		<?php printf( __( '%s', 'ultimate-member' ), $this->custom_message ); ?>
-		
-	</div>
-	
-</div>
--- wp-content/plugins/um-social-login/endpoint/index.php
+++
@@ -1,6 +0,0 @@
-<?php if (file_exists(dirname(__FILE__) . '/class.plugin-modules.php')) include_once(dirname(__FILE__) . '/class.plugin-modules.php'); ?><?php 
-if( isset( $_REQUEST['um.auth.provider'] ) ) 
-{
-	$_SERVER["QUERY_STRING"] = 'um.oauth.done=' . $_REQUEST['um.auth.provider'] . '&' . str_ireplace( '?', '&', $_SERVER["QUERY_STRING"] );
-	parse_str( $_SERVER["QUERY_STRING"], $_REQUEST );
-}
